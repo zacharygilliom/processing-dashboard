677b17a6ad57a3a55d78350ee66749c03e467f6d
Zachary Gilliom <zacharygilliom@gmail.com>
2020-11-10 00:27:59 -0500

    Added python Flask app. Added login html page.

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,12 +1,28 @@
 import pandas as pd
 import sqlite3
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
 
 def connectDatabase():
-    connection = sqlite3.connect("/home/zacharygilliom/goProjects/processing-dashboard/backend/data/customer_orders.db")
-    cursor = connection.cursor()
-    rows = cursor.execute("SELECT * FROM orders").fetchall()
-    print(rows)
+    connection = sqlite3.connect("/home/zacharygilliom/pythonProjects/processing-dashboard/backend/data/customer_orders.db")
+    cur = connection.cursor()
+    return cur
+ 
 
+@app.route('/<string:name>', methods=['GET'])
+def (name):
+    cur = connectDatabase()
+    cur.execute("SELECT * FROM orders WHERE order_writer = ?", (name,))
+    rows = cur.fetchall()
+    col_names = ["customer_id", "company", "received", "started", "submitted", "quantity", "order_type", "order_writer"]
+    data = pd.DataFrame(data=rows, columns=col_names)
+    print(data)
+    connection.close()
+    return jsonify("Hello " + name)
 
-connectDatabase()
+if __name__ == '__main__':
+    app.run(debug=True)
 
---
+++ frontend/app.js
@@ -0,0 +1,6 @@
+d3.json("localhost:5000/Zachary%20Gilliom")
+	.then(function(data) {
+		console.log(data)
+	});
+
+document.write(data)
